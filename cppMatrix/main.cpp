e4d68568500261b77fa289125d74f607a8c756e0
Joni Juntunen <Joni.M.Juntunen@student.lamk.fi>
2018-04-20 15:07:45 +0300

    menues done, quick & dirty

--- cppMatrix/main.cpp
+++ cppMatrix/main.cpp
@@ -21,35 +21,95 @@
 
 using namespace std;
 
+map<int,Matrix> matrixes;
 /*
  * 
  */
 int main(int argc, char** argv)
 {
     void printMenu();
+    void printMatrixMenu(Matrix *matrix);
+    void printSelectMatrix();
     
     string input;
-    map<int,Matrix> matrixes;
     
     while (true)
     {
         printMenu();
         cin >> input;
         transform(input.begin(), input.end(), input.begin(), ::tolower);
-        if (input == "c")
-        {
-            matrixes.insert(pair<int,Matrix>(matrixes.end()->first + 1, Matrix()));
-        }
-        else if(input == "l")
-        {
-            for (map<int,Matrix>::iterator it = matrixes.begin(); it != matrixes.end(); it++) 
-            {
-                cout << it->first << ": " << it->second.toString() << endl;
-            }
-        }
         if (input == "q")
         {
             exit(0);
+        }
+        else if (input == "c")
+        {
+            Matrix matrix = Matrix();
+            matrixes.insert(pair<int,Matrix>(matrixes.end()->first + 1, matrix));
+        }
+        else if(input == "l")
+        {
+            while (true)
+            {
+                printSelectMatrix();
+                cin >> input;
+                transform(input.begin(), input.end(), input.begin(), ::tolower);
+                if (input == "q")
+                {
+                    break;
+                }
+                else
+                {
+                    Matrix *target = &matrixes.at(atoi(input.c_str()));
+                    while (true)
+                    {
+                        printMatrixMenu(target);
+                        cin >> input;
+                        transform(input.begin(), input.end(), input.begin(), ::tolower);
+                        if (input == "q")
+                        {
+                            break;
+                        }
+                        else if (input == "a")
+                        {
+                            int x, y, v;
+                            string xyv;
+                            cout << "x: ";
+                            cin >> xyv;
+                            x = atoi(xyv.c_str());
+                            cout << "y: ";
+                            cin >> xyv;
+                            y = atoi(xyv.c_str());
+                            cout << "value: ";
+                            cin >> xyv;
+                            v = atoi(xyv.c_str());
+                            target->set(x, y, v);
+                        }
+                        else if (input == "m")
+                        {
+                            int m;
+                            string in;
+                            cout << "multiplier: ";
+                            cin >> in;
+                            m = atoi(in.c_str());
+                            target->product(m);
+                        }
+                        else if (input == "s")
+                        {
+                            string select;
+                            int selection;
+                            printSelectMatrix();
+                            cin >> select;
+                            selection = atoi(select.c_str());
+                            target->sum(matrixes.at(selection));
+                        }
+                        else if (input == "p")
+                        {
+                            target->print();
+                        }
+                    }
+                }
+            }
         }
     }
 }
@@ -67,3 +127,33 @@ void printMenu()
     cout << menu.str();
 }
 
+void printMatrixMenu(Matrix *matrix)
+{
+    stringstream menu;
+    menu << "MATRIX " << matrix->toString() << endl;
+    menu << "========" << endl;
+    menu << "(A). Add value" << endl;
+    menu << "(M). Mulptiply values" << endl;
+    menu << "(S). Sum matrix" << endl;
+    menu << "(P). Print" << endl;
+    menu << "(Q). << Back" << endl;
+    menu << endl;
+    menu << "Command: ";
+    cout << menu.str();
+}
+
+void printSelectMatrix()
+{
+    stringstream menu;
+    menu << "SELECT MATRIX" << endl;
+    menu << "========" << endl;
+    for (map<int,Matrix>::iterator it = matrixes.begin(); it != matrixes.end(); it++) 
+    {
+        menu << it->first << ": " << it->second.toString() << endl;
+    }
+    menu << "=======" << endl;
+    menu << endl;
+    menu << "Select matrix" << endl;
+    menu << "(Q). << Back" << endl;
+    cout << menu.str();
+}
