272ab6357c0799f387fd5f43f2f9bed194efc42f
sampoharjula <sampo.harjula@student.lamk.fi>
2018-04-20 11:33:54 +0300

    New Classes

---
+++ cppMatrix/Point.cpp
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Point.cpp
+ * Author: Sampo
+ * 
+ * Created on 20. huhtikuuta 2018, 11:18
+ */
+
+#include "Point.h"
+
+Point::Point(int x, int y) {
+}
+
+Point::~Point() {
+}
