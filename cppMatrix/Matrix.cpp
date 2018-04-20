75feb904d089b096c808c2fd999ce0e15e495140
sampoharjula <sampo.harjula@student.lamk.fi>
2018-04-20 15:38:41 +0300

    remove cout

--- cppMatrix/Matrix.cpp
+++ cppMatrix/Matrix.cpp
@@ -50,7 +50,6 @@ bool Matrix::sum(Matrix *o){
     if (this->maxRows == o->maxRows && this->maxCols == o->maxCols){
         for (int j=0; j<this->getRowCount(); j++){
             for (int i=0; i<this->getColCount(); i++){       
-                cout << this->get(i,j) << " : " << o->get(i,j) << endl;
                 this->set(i,j, this->get(i,j) + o->get(i,j));
             }
         }        
